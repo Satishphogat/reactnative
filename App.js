b868e9ceae72cbf40976fcf0931466fb1719e187
satishphogat <satish.phogat@kelltontech.com>
2020-01-28 18:33:48 +0530

    worked on login ui.

--- App.js
+++ App.js
@@ -15,11 +15,19 @@ import {
   Text,
   StatusBar,
   TouchableHighlight,
-  Image
+  Image,
+  TextInput,
+  Button
   } from 'react-native';
 
 
   export default class Login extends Component {
+
+
+        state = {
+            email: '',
+            password: '',
+        }
 
 onClickListener(type) {
   console.log(type);
@@ -27,6 +35,7 @@ onClickListener(type) {
 
     render() {
       return (
+        <ScrollView>
         <View style={style.container}>
 
         <TouchableHighlight
@@ -51,7 +60,25 @@ onClickListener(type) {
 
         </View>
 
+        <View>
+        <TextInput style={{}} placeholder = "Email Id" style={style.textField} onChangeText = {(email) =>
+          this.setStage({email: email})} value={this.state.email} />
+        <TextInput style={{}} placeholder = "Password" style={style.textField} onChangeText = {(password) => this.setStage({password: password}) } value={this.state.password} />
+
+        <TouchableHighlight style={style.login} onPress = {() => this.onClickListener('login')}>
+        <Text style={{textAlign: 'center', color: '#ffffff', fontSize: 22}}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={style.forgotPassword} onPress = {() => this.onClickListener('forgotpassword')}>
+        <Text style={{textAlign: 'center', color: '#a9a9a9', fontSize: 22}}>Forgot Password?</Text>
+        </TouchableHighlight>
+
         </View>
+
+
+
+        </View>
+        </ScrollView>
         );
       }
     }
@@ -68,6 +95,7 @@ onClickListener(type) {
           flexDirection: 'row',
           position: 'absolute',
           right: 10,
+          fontWeight: 'bold'
         },
         squareBorderButton: {
           alignItems: 'center',
@@ -76,5 +104,29 @@ onClickListener(type) {
           flex: 2,
           borderWidth: 1,
           borderColor: '#a9a9a9',
+        },
+        textField: {
+          height: 70,
+          top: 160,
+          color: '#a9a9a9',
+          fontSize: 22,
+          textAlign: 'center',
+           borderBottomWidth: 1,
+           borderBottomColor: '#a9a9a9',
+           marginHorizontal: 30
+        },
+        login: {
+          top: 180,
+          marginHorizontal: 30,
+          height: 50,
+          backgroundColor: '#FF982C',
+          justifyContent: 'center',
+          borderRadius: 5
+        },
+        forgotPassword: {
+          top: 180,
+          marginHorizontal: 30,
+          height: 50,
+          justifyContent: 'center'
         }
         })
